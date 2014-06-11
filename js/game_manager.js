0450396299cbc752c4ed4f7f393239d2ddf1840d
Vincent Povirk <madewokherd@gmail.com>
2014-06-10 20:51:26 -0500

    hack hack cough wheeze

--- js/game_manager.js
+++ js/game_manager.js
@@ -68,7 +68,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.floor(Math.random() * 3 + 1);
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -126,6 +126,27 @@ GameManager.prototype.moveTile = function (tile, cell) {
   tile.updatePosition(cell);
 };
 
+isFib = function (m, n) {
+  if (m < n)
+  {
+    var temp = n;
+    n = m;
+    m = temp;
+  }
+  var a = 1;
+  var b = 1;
+  while (b < m)
+  {
+    var temp = a + b;
+    a = b;
+    b = temp;
+  }
+  if (a == n)
+    return true;
+  else
+    return false;
+}
+
 // Move tiles on the grid in the specified direction
 GameManager.prototype.move = function (direction) {
   // 0: up, 1: right, 2: down, 3: left
@@ -153,8 +174,8 @@ GameManager.prototype.move = function (direction) {
         var next      = self.grid.cellContent(positions.next);
 
         // Only one merger per row traversal?
-        if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+        if (next && isFib(next.value, tile.value) && !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value + next.value);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -256,7 +277,7 @@ GameManager.prototype.tileMatchesAvailable = function () {
 
           var other  = self.grid.cellContent(cell);
 
-          if (other && other.value === tile.value) {
+          if (other && isFib(other.value, tile.value)) {
             return true; // These two tiles can be merged
           }
         }
